24b048e612f617754102d1f686c0dcf58aae31be
yasminebounasla <162790511+yasminebounasla@users.noreply.github.com>
2024-12-20 17:26:26 +0100

    Update composition.h

--- composition.h
+++ composition.h
@@ -1,19 +1,24 @@
 #ifndef COMPOSITION_H  
 #define COMPOSITION_H
 #include <string.h> 
-#include <time.h>
 
-// struct for the composition 
+// struct for the date
+typedef struct date {
+    int jour;
+    int mois;
+    int annee;
+} date;
 
+//struct for composition
 typedef struct {
 
     int ID;
     char name[20];
     char composer[20];
     char statu[10];
-    char property[10];
-    time_t date_cretion;
-    time_t modification; 
+    int property;
+    date date_cretion;
+    date modification; 
 
 } composition;
 
