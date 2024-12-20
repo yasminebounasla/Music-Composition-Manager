143ebcd3cd344d24e97f28b76c12fa0dddb72450
yasminebounasla <162790511+yasminebounasla@users.noreply.github.com>
2024-12-20 17:33:23 +0100

    Update cree_historique.c

--- cree_historique.c
+++ cree_historique.c
@@ -1,10 +1,9 @@
 #include "historique.h"
 #include "composition.h"
-#include <time.h>
 #include <stdio.h>
 #include <string.h>
 
-list_histo cree_historique(list_histo l, composition compo, char action[10], char field[50], char befor[50], char after[50]){
+list_histo cree_historique(list_histo l, composition compo, char action[10], char field[50], char befor[50], char after[50], date D){
     list_histo new, p;
 
     new =(list_histo)malloc(sizeof(cell));
@@ -15,7 +14,9 @@ list_histo cree_historique(list_histo l, composition compo, char action[10], cha
     strcpy(new->info.modified_field ,field);
     strcpy(new->info.before_modification ,befor);
     strcpy(new->info.after_modification ,after);
-    new->info.modification_date =time(NULL);
+    new->info.modification_date.jour = D.jour;
+    new->info.modification_date.mois = D.mois;
+    new->info.modification_date.annee = D.annee;
     new->next = NULL;
 
     if( l == NULL){
